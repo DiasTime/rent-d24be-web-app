1fc45e363c0cec6cb1f53b954d01bed895b51cfe
DiasTime <36094372+DiasTime@users.noreply.github.com>
2025-06-20 17:38:42 +0500

    я

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('text appirience kz', async ({ page }) => {
-  await page.goto('https://rent-d24be.web.app/signin');
+  await page.goto('https://rent-d24be.web.app');
   await page.getByTestId('navigate-sign-up').click();
   await expect(page.locator('#root')).toContainText('ЕсіміЕсімі');
   await expect(page.locator('#root')).toContainText('ТегіТегі');
@@ -11,11 +11,13 @@ test('text appirience kz', async ({ page }) => {
   await expect(page.locator('#root')).toContainText('Аккаунтыңыз бар ма? Кіру');
   await expect(page.getByTestId('sign-up-text')).toContainText('Тіркелу');
   await expect(page.getByRole('heading')).toContainText('Тіркелу');
+  await page.screenshot({ fullPage: true });
+
 });
 
 
 test('text appirience ru', async ({ page }) => {
-  await page.goto('https://rent-d24be.web.app/signup');
+  await page.goto('https://rent-d24be.web.app');
   await page.getByRole('button', { name: 'Kaz' }).click();
   await page.getByTestId('navigate-sign-up').click();
   await expect(page.locator('#root')).toContainText('ИмяИмя');
@@ -26,12 +28,15 @@ test('text appirience ru', async ({ page }) => {
   await expect(page.locator('#root')).toContainText('Есть аккаунт? Войти');
   await expect(page.getByTestId('sign-up-text')).toContainText('Зарегистрироваться');
   await expect(page.getByRole('heading')).toContainText('Зарегистрироваться');
+  await page.screenshot({ fullPage: true });
+
 });
 
 
 
 test('is clicable', async ({ page }) => {
-  await page.goto('https://rent-d24be.web.app/signin');
+  await page.goto('https://rent-d24be.web.app');
+  await page.getByRole('button', { name: 'Kaz' }).click();
   await page.getByTestId('navigate-sign-up').click();
   await page.locator('#firstName').click();
   await page.locator('#lastName').click();
@@ -46,21 +51,26 @@ test('is clicable', async ({ page }) => {
   await page.getByRole('button').filter({ hasText: '󰖙' }).click();
   await page.getByRole('button').filter({ hasText: '󰖙󰖔' }).click();
   await page.getByRole('link', { name: 'Войти, back' }).click();
+  await page.screenshot({ fullPage: true });
+
 });
 
 test('registration', async ({ page }) => {
-  await page.goto('https://rent-d24be.web.app/signin');
+  await page.goto('https://rent-d24be.web.app');
+  await page.getByRole('button', { name: 'Kaz' }).click();
   await page.getByTestId('navigate-sign-up').click();
   await page.locator('#firstName').click();
   await page.locator('#firstName').fill('тестовое имя');
   await page.locator('#lastName').click();
   await page.locator('#lastName').fill('тестовое фамилия');
   await page.getByTestId('email-sign-up').click();//изменить здесь цифру
-  await page.getByTestId('email-sign-up').fill('dias.tester1@gmal.com');
+  await page.getByTestId('email-sign-up').fill('dias.tester4@gmal.com');
   await page.getByTestId('password-sign-up').click();
   await page.getByTestId('password-sign-up').fill('Qweasdzxcqw13d#');
   await page.getByTestId('confirm-password-sign-up').click();
   await page.getByTestId('confirm-password-sign-up').fill('Qweasdzxcqw13d#');
   await page.getByTestId('sign-up').click();
   await expect(page.getByRole('heading')).toContainText('Присоединиться в компанию');
+  await page.screenshot({ fullPage: true });
+
 });
